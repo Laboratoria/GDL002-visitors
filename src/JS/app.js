86f49cf185d548c824be81203e6e4550d5cace50
Lexavil <aviles.esp@gmail.com>
2019-05-06 10:56:14 -0500

    se quito el arreglo de la foto

--- src/JS/app.js
+++ src/JS/app.js
@@ -147,23 +147,6 @@ navigator.mediaDevices.getUserMedia({
 
 
 
-// //jan
-// document.getElementById("hiddenPicture").style.display= "none";
-
-// var player = document.getElementById('player');
-// var snapshotCanvas = document.getElementById('snapshot');
-// var captureButton = document.getElementById('capture');
-
-// var handleSuccess = function(stream) {
-// // Attach the video stream to the video element and autoplay.
-// player.srcObject = stream;
-// };
-
-// captureButton.addEventListener('click', function() {
-// var context = snapshotCanvas.getContext('2d');
-// // Draw the video frame to the canvas.
-// const showImage = context.drawImage(player, 0, 0, snapshotCanvas.width,
-//     snapshotCanvas.height);
 
 // const imageData = context.getImageData(0, 0, snapshotCanvas.width,snapshotCanvas.height);
 // console.log(imageData);
